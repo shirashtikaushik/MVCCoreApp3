9b91c4a6a3c4ba8a67856814ed2375f951df055c
admin <admin@GEDU-LP313>
2024-01-17 22:56:44 +0530

    Add project files.

---
+++ Controllers/SumController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCoreApp3.Models;
+
+
+namespace MVCCoreApp3.Controllers
+{
+    public class SumController : Controller
+    {
+       
+        [HttpGet]
+        public IActionResult Add()
+        {
+            Numbers numbers = new Numbers();
+            return View(numbers);
+        }
+        [HttpPost]
+        public IActionResult Add(Numbers numbers)
+        {
+            ViewBag.res = numbers.Num1 + numbers.Num2;
+            return View();
+
+        }
+    }
+}
+
+
+﻿
---
+++ Models/Numbers.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCCoreApp3.Models
+{
+    public class Numbers
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+    }
+}
+
+
